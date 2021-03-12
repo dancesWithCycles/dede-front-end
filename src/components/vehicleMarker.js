dae7f344716ecfad126dae1fd7a11aea22c13203
Stefan Begerad <sbegerad@posteo.de>
2021-03-11 23:02:26 -0500

    adjusted marker age to 10 minutes

--- src/components/vehicleMarker.js
+++ src/components/vehicleMarker.js
@@ -18,27 +18,24 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import React from 'react';
 import VehicleIcon from './vehicleIcon';
 import VehiclePopup from './vehiclePopup';
-import {CircleMarker} from 'react-leaflet';
+import {Marker} from 'react-leaflet';
 
 const VehicleRealTimeMarker=(props)=>{
     const {location}=props;
     //TODO improve availability of age property
-    const age=120000;
+    const age=600000;
     let sysTs=Date.now();
     let diff=sysTs-location.ts;
     if(diff<age){
 	return(
 		<>
-		<CircleMarker
-	    color='#3F00FF'
-            opacity={1}
-            radius={10}
-            weight={1}
-            center={[location.lat,location.lon]}
+		<Marker
 	    key={location.uuid}
+	    position={[location.lat,location.lon]}
+	    icon={VehicleIcon(location)}
 		>
 		<VehiclePopup location={location}/>
-	    </CircleMarker>
+	    </Marker>
 	    </>
 	);
     }else{
