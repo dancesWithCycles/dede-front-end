d498059cb50198fbc5f5b3cfa6e293e171d66a28
Begerad, Stefan <dialog@swingbe.de>
2022-02-03 08:41:31 -0500

    chore: remove debugging log

--- src/components/vehicleMarker.js
+++ src/components/vehicleMarker.js
@@ -26,12 +26,6 @@ const VehicleRealTimeMarker=(props)=>{
     const age=600000;
     let sysTs=Date.now();
     let diff=sysTs-location.ts;
-    console.log('VehRTMarker: location.ts: '
-		+location.ts
-		+', sysTs: '
-		+sysTs
-		+', diff: '
-		+diff);
     //vehicles with movement data age bigger than shall not be presented
     if(diff<age){
 	return(
